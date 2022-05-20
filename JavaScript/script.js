96148c5f5f4e7aef56feef838cd260f12bc1ee58
Lidor <101148504+L02C@users.noreply.github.com>
2022-05-20 15:10:21 +0200

    Finalised script

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,13 @@
 const interactions = document.querySelectorAll('.blog-interaction');
 
-interactions.forEach(interaction => {
+interactions.forEach(touchstart => {
   interaction.addEventListener('click', function handleClick(event) {
     console.log('interaction clicked', event);
     interaction.classList.toggle("blog-interaction-mobile");
   });
 });
 
-document.addEventListener("click", function remove(event){
+document.addEventListener("touchstart", function remove(event){
   interactions.forEach(interaction => {
     interaction.classList.remove("blog-interaction-mobile");
   })
