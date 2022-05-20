d4d0aff3d311a29af3b849a7963158639dbdfff5
Lidor <101148504+L02C@users.noreply.github.com>
2022-05-20 14:56:38 +0200

    Fixed script
    When document is clicked, push down blog interactions.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,3 +6,9 @@ interactions.forEach(interaction => {
     interaction.classList.toggle("blog-interaction-mobile");
   });
 });
+
+document.addEventListener("click", function remove(event){
+  interactions.forEach(interaction => {
+    interaction.classList.remove("blog-interaction-mobile");
+  })
+})
